224aa5c8d6a41c2e93d4203df6ad5c7294a641f7
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2023-04-27 21:57:12 +0900

    [Gold V] Title: 문자열 게임 2, Time: 228 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -6,7 +6,7 @@ t = int(input())
 for _ in range(t):
     s = input() # 문자열
     k = int(input())
-    ans = [10001,0] # 최소, 최대 길이
+    ans = [] # 문자열의 길이를 담을 리스트
 
     # 문자열에서 문자별로 위치를 저장함
     words = defaultdict(list)
@@ -14,19 +14,16 @@ for _ in range(t):
         words[s[i]].append(i)
 
     # 문자별로 k개 이상인 경우만 최소 혹은 최대 연속 문자열 길이를 구함
-    for word in words.keys():
-        n = len(words[word]) # 단어의 개수
-        if len(words[word]) < k:
+    for word in words.values():
+        n = len(word) # 단어의 개수
+        if n < k:
             continue
         for i in range(n-k+1): # 딱 k 개만 포함하는 문자열
             # 문자열의 길이
-            temp = words[word][i+k-1]-words[word][i]+1
-            if ans[0] > temp:
-                ans[0] = temp
-            if ans[1] < temp:
-                ans[1] = temp
+            temp = word[i+k-1] - word[i] + 1
+            ans.append(temp)
 
-    if ans == [10001,0]: # 최소 최대가 변함이 없다면 답이 없다는 뜻임
-        print(-1)
+    if ans: # 최소 최대가 변함이 없다면 답이 없다는 뜻임
+        print(min(ans),max(ans))
     else:
-        print(*ans)
+        print(-1)
