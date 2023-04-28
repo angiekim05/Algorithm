b6c8efd5f63a6551a7fe75e70315861653c30c5b
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2023-04-28 21:38:42 +0900

    [level 3] Title: 숫자 게임, Time: 53.62 ms, Memory: 18.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,13 @@
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort()
+    i,j = 0,0
+    while j < len(B):
+        if A[i] < B[j]:
+            answer += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+    return answer
