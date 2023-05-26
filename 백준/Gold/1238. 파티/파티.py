21500a6d9b8b8badf6ed4f6a7b40b35f4b32591e
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2023-05-27 01:21:52 +0900

    [Gold III] Title: 파티, Time: 360 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,52 +1,46 @@
+from heapq import heappop, heappush
 import sys
 input = sys.stdin.readline
-from heapq import heappush,heappop
 
-n,m,x=map(int,input().split())
-g = [[] for _ in range(n+1)]
-for _ in range(m):
-    s,e,t = map(int,input().split())
-    #단방향
-    g[s].append((e,t))
+n,m,x = map(int, input().split())
+d = {i+1:[] for i in range(n)}
+for i in range(m):
+    a,b,c = map(int, input().split())
+    d[a].append((b,c))
 
-# 다익스트라 start에서 모든 node까지
-def dijkstra(start):
-    time = [float("inf")]*(n+1)
-    time[start]=0
-    q = []
-    heappush(q,(0,start))
+def bfs_from_x(i):
+    visited = [float("inf")]*(n+1)
+    visited[i] = 0
+    q = [(0,i)]
     while q:
-        t,node = heappop(q)
-        if time[node] > t:
-            break
-        for next_node, next_time in g[node]:
-            cost = t + next_time
-            if time[next_node] > cost:
-                time[next_node] = cost
-                heappush(q,(cost,next_node))
-    return time
+        t,now = heappop(q)
+        if visited[now] > t:
+            continue
+        for next_,t_ in d[now]:
+            if visited[next_] > t+t_:
+                visited[next_] = t+t_
+                heappush(q,(t+t_,next_))
+    return visited
 
-# 다익스트라 start에서 end node까지
-def dijkstra_to(start,end):
-    time = [float("inf")]*(n+1)
-    time[start]=0
-    q = []
-    heappush(q,(0,start))
+def bfs_from_i(i,x):
+    visited = [float("inf")]*(n+1)
+    visited[i] = 0
+    q = [(0,i)]
     while q:
-        t,node = heappop(q)
-        if node == end:
+        t,now = heappop(q)
+        if now == x:
             break
-        for next_node, next_time in g[node]:
-            cost = t + next_time
-            if time[next_node] > cost:
-                time[next_node] = cost
-                heappush(q,(cost,next_node))
-    return time
+        if visited[now] > t:
+            continue
+        for next_,t_ in d[now]:
+            if visited[next_] > t+t_:
+                visited[next_] = t+t_
+                heappush(q,(t+t_,next_))
+    return visited[x]
 
-to_x = dijkstra(x)
-to_x[0] = 0
-for i in range(n):
-    if i+1 == x:
-        continue
-    to_x[i+1]+=dijkstra_to(i+1,x)[x]
-print(max(to_x))
+ans = 0
+from_x_to_i = bfs_from_x(x)
+for i in range(1,n+1):
+    ans = max(ans,bfs_from_i(i,x)+from_x_to_i[i])
+
+print(ans)
