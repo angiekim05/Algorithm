7dcb84ba60c6c1cf597fbff737527a3f47be427d
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2023-06-08 19:10:51 +0900

    [Gold IV] Title: 좋다, Time: 176 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -5,24 +5,20 @@ n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 ans = 0
-if len(arr) < 3:
-    print(0)
-else:
-    for tgt_idx in range(n):
-        i = 0 if tgt_idx > 0 else 1
-        j = n-1 if tgt_idx != n-1 else n-2
+for tgt_idx in range(n):
+    i = 0 if tgt_idx > 0 else 1
+    j = n-1 if tgt_idx != n-1 else n-2
+    while i < j:
         s = arr[i] + arr[j]
-        while i < j:
-            if s == arr[tgt_idx]:
-                ans += 1
-                break
-            elif s < arr[tgt_idx]:
+        if s == arr[tgt_idx]:
+            ans += 1
+            break
+        elif s < arr[tgt_idx]:
+            i += 1
+            if i == tgt_idx:
                 i += 1
-                if i == tgt_idx:
-                    i += 1
-            else:
+        else:
+            j -= 1
+            if j == tgt_idx:
                 j -= 1
-                if j == tgt_idx:
-                    j -= 1
-            s = arr[i] + arr[j]
-    print(ans)
+print(ans)
