1c775b17a7d71ee44c2fde9a8d44732124d0d389
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-01-06 20:36:25 +0900

    [Silver I] Title: 유성, Time: 1464 ms, Memory: 132264 KB -BaekjoonHub

--- 백준/Silver/10703. 유성/유성.py
+++ 백준/Silver/10703. 유성/유성.py
@@ -3,39 +3,33 @@ input = sys.stdin.readline
 print = sys.stdout.write
 n,m = map(int, input().split())
 arr = [input() for _ in range(n)]
+dist = 3001
+for j in range(m):
+    temp = -3001 # 만약 유성이 없으면
+    for i in range(n):
+        if arr[i][j] == "X":
+            temp = i
+        elif arr[i][j] == "#":
+            dist = min(i-temp-1,dist)
+            break
 
-def get_dist():
-    dist = n
+for i in range(min(n,dist)):
+    res = ""
     for j in range(m):
-        temp = -3001 # 만약 유성이 없으면
-        for i in range(n):
-            if arr[i][j] == "X":
-                temp = i
-            elif arr[i][j] == "#":
-                dist = min(i-temp-1,dist)
-                break
-    return dist
+        if arr[i][j] == "#":
+            res += "#"
+        else:
+            res += "."
+    print(res+"\n")
 
-def restore():
-    dist = get_dist()
-    for i in range(dist):
-        res = ""
-        for j in range(m):
-            if arr[i][j] == "#":
-                res += "#"
-            else:
-                res += "."
-        print(res+"\n")
 
-    for i in range(dist, n):
-        res = ""
-        for j in range(m):
-            if arr[i-dist][j] == "X":
-                res += "X"
-            elif arr[i][j] == "#":
-                res += "#"
-            else:
-                res += "."
-        print(res+"\n")
-
-restore()
+for i in range(dist, n):
+    res = ""
+    for j in range(m):
+        if arr[i-dist][j] == "X":
+            res += "X"
+        elif arr[i][j] == "#":
+            res += "#"
+        else:
+            res += "."
+    print(res+"\n")
