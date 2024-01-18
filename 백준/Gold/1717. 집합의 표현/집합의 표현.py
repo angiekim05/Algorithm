d83d84672d6abc3320b39a8ab8445e460d1daa8a
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-01-18 21:46:34 +0900

    [Gold V] Title: 집합의 표현, Time: 304 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+n,m = map(int,input().split())
+
+group = list(range(n+1))
+
+def find(x):
+    a = x
+    while group[a] != a:
+        a = group[a]
+    group[x] = a
+    return group[x]
+
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x <= y:
+        group[y] = x
+    else:
+        group[x] = y
+
+for _ in range(m):
+    p,a,b = map(int,input().split())
+    if p == 0:
+        union(a,b)
+    else:
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
