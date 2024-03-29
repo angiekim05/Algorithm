8c9184d20695bcebd72c6cd5c649f4a5048c41c8
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-03-29 22:58:56 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.21 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,6 +1,7 @@
 def solution(today, terms, privacies):
     answer = []
-    yyyy, mm, dd = map(int, today.split("."))
+    y, m, d = map(int, today.split("."))
+    today = y*12*28 + m*28 + d
     
     term = dict()
     for t in terms:
@@ -11,18 +12,9 @@ def solution(today, terms, privacies):
         numb = idx+1
         date, type_ = p.split()
         y, m, d = map(int, date.split("."))
-        temp,m = divmod(m+term[type_],12)
-        y += temp
-        if m == 0:
-            m = 12
-            y -= 1
-        if yyyy > y:
+        m += term[type_]
+        
+        if y*12*28 + m*28 + d <= today:
             answer.append(numb)
-        elif yyyy == y:
-            if mm > m:
-                answer.append(numb)
-            elif mm == m:
-                if dd >= d:
-                    answer.append(numb)
     
     return answer
