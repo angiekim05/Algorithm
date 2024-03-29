423178a7c784a0f0ce60b2beb130d80921eb52d1
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-03-29 23:17:49 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,3 +1,4 @@
+# 날짜를 일수로 변환
 def to_days(date):
     y, m, d = map(int, date.split("."))
     return y*12*28 + m*28 + d
@@ -6,16 +7,19 @@ def solution(today, terms, privacies):
     answer = []
     today = to_days(today)
     
+    # 약관 정보
     term = dict()
     for t in terms:
         type_, period = t.split()
         term[type_] = int(period)*28
     
+    # 개인정보 만료기간과 오늘 일수를 비교하여 파기 목록에 추가
     for idx, p in enumerate(privacies):
         numb = idx+1
         date, type_ = p.split()
         
-        if to_days(date) + term[type_] <= today:
+        # 오늘보다 만료기간이 짧으면 파기해야 한다.
+        if to_days(date) + term[type_] -1 < today:
             answer.append(numb)
     
     return answer
