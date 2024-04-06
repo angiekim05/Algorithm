989b646dca9c5702141ac6c966ec5a8974d8646e
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-04-07 01:51:42 +0900

    [level 4] Title: 호텔 방 배정, Time: 412.29 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,17 +1,36 @@
+import sys
+sys.setrecursionlimit(10**8)
 def solution(k, room_number):
     answer = []
-    next_room = dict()
+    nr = dict()
+    def find_nr(x):
+        if x not in nr:
+            nr[x] = x+1
+            return x
+        nr[x] = find_nr(nr[x])
+        return nr[x]
+    
     for x in room_number:
-        if x not in next_room:
-            next_room[x] = x+1
-            answer.append(x)
-        else:
-            temp = []
-            while x in next_room:
-                temp.append(x)
-                x = next_room[x]
-            next_room[x] = x+1
-            answer.append(x)
-            for t in temp:
-                next_room[t] = x+1
+        answer.append(find_nr(x))
     return answer
+
+
+
+
+# def solution(k, room_number):
+#     answer = []
+#     next_room = dict()
+#     for x in room_number:
+#         if x not in next_room:
+#             next_room[x] = x+1
+#             answer.append(x)
+#         else:
+#             temp = []
+#             while x in next_room:
+#                 temp.append(x)
+#                 x = next_room[x]
+#             next_room[x] = x+1
+#             answer.append(x)
+#             for t in temp:
+#                 next_room[t] = x+1
+#     return answer
