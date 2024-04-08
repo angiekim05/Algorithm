fa1b61a016c3024cc53d976454881a188718ef59
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-04-08 15:44:18 +0900

    [Gold II] Title: 보석 도둑, Time: 732 ms, Memory: 106236 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,19 +1,24 @@
 import heapq
+from collections import defaultdict
 import sys
 input = sys.stdin.readline
 n,k = map(int, input().split())
-gems = [tuple(map(int, input().split())) for _ in range(n)]
-bags = [int(input()) for _ in range(k)]
-gems.sort(reverse=True)
+# 동일 무게에 대해 묶음으로써 동시에 포함되지 않는 무게의 보석들을 제거 가능
+gems = defaultdict(list)
+for _ in range(n):
+    m,v = map(int, input().split())
+    gems[m].append(v)
+# 보석의 최대 무게가 1000000라서 더 큰 가방을 볼 필요 없음
+bags = [min(1000000,int(input())) for _ in range(k)]
+gems_w = sorted(gems.keys(),reverse=True)
 bags.sort()
 pq = []
-answer = 0
-for x in bags:
-    while gems:
-        if gems[-1][0] > x:
-            break
-        nm,nv = gems.pop()
-        heapq.heappush(pq,-nv)
-    if pq:
-        answer -= heapq.heappop(pq)
-print(answer)
+for w in gems_w:
+    for v in gems[w]:
+        if bags and bags[-1] >= w:
+            bags.pop()
+            heapq.heappush(pq,v)
+        elif pq and v > pq[0]:
+            heapq.heappop(pq)
+            heapq.heappush(pq,v)
+print(sum(pq))
