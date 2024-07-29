c4b382d857deb19d0584655ca2f1eb1a749067b4
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-07-30 02:30:06 +0900

    [Gold IV] Title: 도둑, Time: 240 ms, Memory: 162456 KB -BaekjoonHub

--- 백준/Gold/13422. 도둑/도둑.py
+++ 백준/Gold/13422. 도둑/도둑.py
@@ -3,23 +3,24 @@ input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
+    # 총 집 개수, 연속으로 훔질 집 개수, 자동 방범 장치 작동 최소 비용
     n,m,k = map(int, input().split())
+    # 돈의 양
     houses = list(map(int, input().split()))
     
     if n == m:
         if sum(houses) < k:
-            print(1)
+            ans = 1
         else:
-            print(0)
-        continue
-    
-    houses += houses
-    ans = 0
-    cur = sum(houses[:m])
-    for i in range(n):
-        if cur < k:
-            ans += 1
-        cur -= houses[i]
-        cur += houses[i+m]
+            ans = 0
+    else:
+        houses += houses
+        ans = 0
+        cur = sum(houses[:m])
+        for i in range(n):
+            if cur < k:
+                ans += 1
+            cur -= houses[i]
+            cur += houses[i+m]
 
     print(ans)
