e51a4d0056cbc590ca1fa349a3acb29cab64ae9d
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-10-16 16:03:14 +0900

    [Gold III] Title: 세 용액, Time: 144 ms, Memory: 110604 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -2,24 +2,28 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
-arr.sort()
-z = float("INF")
-zarr = []
-for k in range(n-2):
-    i,j = k+1, n-1
-    x = arr[k] + arr[i] + arr[j]
-    z_ = abs(x)
-    while i < j < n:
-        if z > z_:
-            z = z_
-            zarr = [arr[k], arr[i], arr[j]]
-        if x > 0:
-            j -= 1
-            x += arr[j] - arr[j+1]
-        else:
-            i += 1
-            x += arr[i] - arr[i-1]
-        z_ = abs(x)
+arr.sort() # 정렬하기
+closeZero = float("INF") # 0과 가장 가까운 합을 저장해 놓을 변수
+res = []   # 최고의 조합을 담을 리스트
 
+for k in range(n-2): # 임의의 용액을 고정
+    left, right = k+1, n-1 # 선택된 용액 이후의 최소값 최대값 위치
 
-print(*zarr)
+    while left < right < n:
+        curSum = arr[k] + arr[left] + arr[right]
+
+        # 현재 합이 0에 더 가까운 경우 업데이트
+        if abs(curSum) < abs(closeZero):
+            closeZero = abs(curSum)
+            res = [arr[k], arr[left], arr[right]]
+
+        # 0이면 최고의 조합임으로 탐색 종료
+        if curSum == 0:
+            print(*res)
+            exit()
+        elif curSum > 0: # 합이 0보다 크면 오른쪽 포인터를 왼쪽으로 이동
+            right -= 1
+        else: # 합이 0보다 작으면 왼쪽 포인터를 오른쪽으로 이동
+            left += 1
+
+print(*res)
