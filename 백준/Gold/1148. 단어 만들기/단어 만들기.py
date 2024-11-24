bc1cf406d8f78056d78bfec3ff4c721e82b639d5
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-11-25 05:18:48 +0900

    [Gold V] Title: 단어 만들기, Time: 1020 ms, Memory: 282692 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -1,59 +1,54 @@
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
-words_dict = dict()
+# 단어 저장
+words_list = []
+
+# 단어 추가 함수
 def add_word(word):
-    temp = dict()
-    for x in word:
-        if x in temp:
-            temp[x] += 1
-        else:
-            temp[x] = 1
-    words_dict[word] = temp
-    return
+    words_list.append(Counter(word))
 
-def can_make_word(puzzle: dict, word: dict):
-    for x in word:
-        if word[x] > puzzle.get(x, 0):
-            return False
-    return True
+# 퍼즐로 단어를 만들 수 있는지 확인
+def can_make_word(puzzle_counter, word_counter):
+    return all(word_counter[char] <= puzzle_counter.get(char, 0) for char in word_counter)
 
+# 결과 출력 함수
 def print_answer(puzzle_count):
-    ans = list(puzzle_count.items())
-    ans.sort(key = lambda x: (x[1],x[0]))
-    min_, max_ = ans[0][1], ans[-1][1]
-    min_alpha, max_alpha = "",""
-    for x, cnt in ans:
-        if cnt == min_:
-            min_alpha += x
-        if cnt == max_:
-            max_alpha += x
-    print(min_alpha, min_, max_alpha, max_)
+    sorted_counts = sorted(puzzle_count.items(), key=lambda x: (x[1], x[0]))
+    
+    # 최소, 최대값 및 해당 알파벳 추출
+    min_count = sorted_counts[0][1]
+    max_count = sorted_counts[-1][1]
+    
+    min_alpha = ''.join([char for char, count in sorted_counts if count == min_count])
+    max_alpha = ''.join([char for char, count in sorted_counts if count == max_count])
+    
+    print(min_alpha, min_count, max_alpha, max_count)
 
+# 단어 입력 처리
 while True:
     word = input().strip()
     if word == "-":
         break
     add_word(word)
 
+# 퍼즐 입력 처리
 while True:
     puzzle = input().strip()
     if puzzle == "#":
         break
 
-    puzzle_dict = dict()
-    for x in puzzle:
-        if x in puzzle_dict:
-            puzzle_dict[x] += 1
-        else:
-            puzzle_dict[x] = 1
+    # 퍼즐 빈도 계산
+    puzzle_counter = Counter(puzzle)
+    puzzle_count = {char: 0 for char in puzzle_counter.keys()}
+    
+    for word_counter in words_list:
+        if can_make_word(puzzle_counter, word_counter):
+            for char in word_counter:
+                if char in puzzle_count:
+                    puzzle_count[char] += 1
 
-    can_make = set()
-    can_not_make = set()
-    puzzle_count = {t:0 for t in puzzle_dict.keys()}
-    for word in words_dict:
-        if can_make_word(puzzle_dict, words_dict[word]):
-            for x in words_dict[word]:
-                puzzle_count[x] += 1
-
+    # 결과 출력
     print_answer(puzzle_count)
